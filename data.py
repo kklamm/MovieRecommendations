009516110a17924d5d723aad8047a77d857a53d6
klammer <karl-klammer@e.mail.de>
2018-10-16 22:24:22 +0200

    Update

--- data.py
+++ data.py
@@ -1,11 +1,59 @@
+from dataclasses import dataclass
+from datetime import datetime
+
 from joblib import Memory
 import numpy as np
-from scipy.sparse import lil_matrix
+import pandas as pd
+from scipy.sparse import coo_matrix
+from tqdm import tqdm
 
+from movies import MovieHandler
 
 memory = Memory(location="~/.joblib/")
 
 
+@dataclass
+class Rating:
+    index: int
+    user_id: int
+    rating: float
+    timestamp: datetime
+
+
 @memory.cache
-def df_to_implicit_sparse(df):
-    pass
+def _df_to_implicit_sparse(df, user_index_mapping, movie_index_mapping):
+    n_users = len(user_index_mapping)
+    n_items = len(movie_index_mapping)
+    mat = lil_matrix((n_users, n_items))
+    users = df.userId.map(user_index_mapping)
+    movies = df.movieId.map(movie_index_mapping)
+    values = np.ones_like(users)
+
+    return coo_matrix((values, (movies, values)))
+
+
+class MovieRatings:
+    def __init__(self, ratings_df, ):
+        self._df = ratings_df
+        self._users = ratings_df.userId.sort_values().unique()
+
+    @classmethod
+    def from_file(cls, filename):
+        ratings_df = pd.read_csv(filename)
+        return cls(ratings_df)
+
+    @property
+    def user_index_mapping(self):
+        if not hasattr(self, "_user_index_mapping"):
+            self._user_index_mapping = {u: i for (i, u) in enumerate(self._users)}
+        return self._user_index_mapping
+
+    @property
+    def index_user_mapping(self):
+        if not hasattr(self, "_index_user_mapping"):
+            self._index_user_mapping = {i: u for (i, u) in self._user_index_mapping.items()}
+        return self._index_user_mapping
+
+
+
+def main():
--- movies.py
+++ movies.py
@@ -1,13 +1,56 @@
 from dataclasses import dataclass
 from typing import List
 
+from joblib import Memory
+import pandas as pd
+
+
+memory = Memory(location="~/joblib", verbose=True)
+
 
 @dataclass
 class Movie:
     index: int
+    movie_id: int
     title: str
     genres: List[str]
 
 
+@memory.cache
 def df_to_movies(df):
-    pass
+    df = df.set_index("movieId").sort_index()
+    movies = []
+    for i, (id_, title, genres) in enumerate(df.itertuples()):
+        movies.append(Movie(i, id_, title, genres))
+    return movies
+
+
+class MovieHandler:
+    def __init__(self, movies_df):
+        self.movies = df_to_movies(movies_df)
+
+    @classmethod
+    def from_file(cls, filename):
+        df = pd.read_csv(filename)
+        return cls(df)
+
+    @property
+    def index_id_mapping(self):
+        if not hasattr(self, "_index_id_mapping"):
+            self._index_id_mapping = {m.index: m.movie_id for m in self.movies}
+        return self._index_id_mapping
+
+    @property
+    def id_index_mapping(self):
+        if not hasattr(self, "_id_index_mapping"):
+            self._id_index_mapping = {movie_id: index for (index, movie_id) in self.index_id_mapping.keys()}
+        return self._id_index_mapping
+
+
+def main():
+    mh = MovieHandler.from_file("ml-20m/movies.csv")
+    breakpoint()
+
+
+if __name__ == '__main__':
+    main()
