cb9577926968203b3d6c031cc1c5aadbe3cc9dbe
klammer <karl-klammer@e.mail.de>
2018-10-22 21:55:29 +0200

    Update optimize function

--- mf.py
+++ mf.py
@@ -2,12 +2,27 @@ import numpy as np
 import q
 
 
-def optimize(P, C, X, Y, lambda_):
+def optimize(R, X, Y, lambda_):
+    P = (R > 0).astype(int)
     YtY = Y.T @ Y
 
     for u in range(P.shape[0]):
-        Cu = np.diag(C[u])
+        Pu = P[u].toarray()[0]
+        Ru = R[u].toarray()[0]
+        Cu = 1 + 40 * Ru
+        Cu = np.diag(Cu)
         I = np.diag(np.ones(X.shape[1]))
-        Cu_minus_I = np.diag(C[u] - 1)
+        Cu_minus_I = Cu - 1
         YtCu_minus_IY = Y.T @ Cu_minus_I @ Y
-        X[u] = np.linalg.inv(YtY + YtCu_minus_IY + lambda_ * I) @ Y.T @ Cu @ P[u]
+        X[u] = np.linalg.inv(YtY + YtCu_minus_IY + lambda_ * I) @ Y.T @ Cu @ Pu
+
+    XtX = X.T @ X
+    for i in range(P.shape[1]):
+        Pi = P[:, i].toarray()[:, 0]
+        Ri = R[:, i].toarray()[:, 0]
+        Ci = 1 + 40 * Ri
+        Ci = np.diag(Ci)
+        I = np.diag(np.ones(X.shape[1]))
+        Ci_minus_I = Ci - 1
+        XtCu_minus_IX = X.T @ Ci_minus_I @ X
+        Y[i] = np.linalg.inv(XtX + XtCu_minus_IX + lambda_ * I) @ X.T @ Ci @ Pi
