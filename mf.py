a4400991297edcd6df2ce1d62fd29b3c001906d6
klammer <karl-klammer@e.mail.de>
2018-12-08 13:49:20 +0100

    Avoid code duplication in als_step

--- mf.py
+++ mf.py
@@ -14,7 +14,7 @@ def loss(R, X, Y, lambda_):
 def als_step(R, X, Y, lambda_, alpha=40):
     P = (R > 0).astype(int)
     YtY = Y.T @ Y
-    for u in trange(P.shape[0]):
+    for u in range(P.shape[0]):
         Pu = P[u].toarray()[0]
         Ru = R[u].toarray()[0]
         Cu = 1 + alpha * Ru
@@ -23,21 +23,14 @@ def als_step(R, X, Y, lambda_, alpha=40):
         YtCuY = YtY + Y.T @ (Cu - np.diag(np.ones(Cu.shape[0]))) @ Y
         X[u] = np.linalg.inv(YtCuY + lambda_ * I) @ Y.T @ Cu @ Pu
 
-    XtX = X.T @ X
-    for i in trange(P.shape[1]):
-        Pi = P[:, i].toarray()[:, 0]
-        Ri = R[:, i].toarray()[:, 0]
-        Ci = 1 + 40 * Ri
-        Ci = np.diag(Ci)
-        I = np.diag(np.ones(Y.shape[1]))
-        XtCiX = XtX + X.T @ (Ci - np.diag(np.ones(Ci.shape[0]))) @ X
-        Y[i] = np.linalg.inv(XtCiX + lambda_ * I) @ X.T @ Ci @ Pi
-
 
 def alternate_least_squares(R, X, Y, lambda_, *, n_optimize=15, show_loss=False):
-    if show_loss:
-        print(loss(R, X, Y, lambda_))
-    for n in trange(n_optimize):
-        als_step(R, X, Y, lambda_)
-        if show_loss:
-            print(loss(R, X, Y, lambda_))
+    with tqdm(total=n_optimize) as pbar:
+        for n in range(n_optimize):
+            als_step(R, X, Y, lambda_)
+            pbar.update(0.5)
+            als_step(R.T, Y, X, lambda_)
+            pbar.update(0.5)
+            if show_loss:
+                l = loss(R, X, Y, lambda_)
+                pbar.set_postfix({"loss": l})
